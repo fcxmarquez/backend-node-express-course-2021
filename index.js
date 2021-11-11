e81d4591301788712d45c14e73ddeb47ca03d98c
Francisco <francisco.marquez.solt@gmail.com>
2021-11-11 16:00:34 -0600

    Api Restful

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, my express server');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('Hello, Im a new endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Milk',
+      price: 1000
+    },
+    {
+      name: 'Sauce',
+      price: 500
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  res.json({
+    name: 'Milk',
+    price: 1000
+  });
+})
+
+app.listen(port, () => {
+  console.log('My port ' + port);
+});
+
