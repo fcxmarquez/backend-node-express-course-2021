0a27fe27694b7fb5c8fa6e20a101149568276fc0
Francisco <francisco.marquez.solt@gmail.com>
2021-11-13 12:50:24 -0600

    Query Params

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker');
+
 const app = express();
 const port = 3000;
 
@@ -11,26 +13,52 @@ app.get('/new-route', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Milk',
-      price: 1000
-    },
-    {
-      name: 'Sauce',
-      price: 500
-    }
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 
 app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Milk',
-    price: 1000
+    price: 1000,
   });
-})
-
-app.listen(port, () => {
-  console.log('My port ' + port);
 });
 
+app.get('/products/filter', (req, res) => {
+  res.send('Hello, Im a new endpoint');
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('There not parameters');
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
